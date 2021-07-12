a88928a4b70c89fc6c11921d404fc0adf93e707c
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-07-12 15:40:26 +0100

    Moved implementation of logging in from Activity to Fragment

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -7,8 +7,11 @@ import android.util.Log
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.navigation.findNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
@@ -22,57 +25,15 @@ import com.udacity.project4.databinding.ActivityAuthenticationBinding
  */
 class AuthenticationActivity : AppCompatActivity() {
 
-    companion object {
-        val TAG = AuthenticationActivity::javaClass.name
-    }
-
-    private lateinit var binding: ActivityAuthenticationBinding
-    private lateinit var registerForActivityResult: ActivityResultLauncher<Intent>
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
-
-        registerForSignInResult()
-
-        binding.loginButton.setOnClickListener {
-            // TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google //DONE
-            launchSignInFlow()
-        }
-//          TODO: If the user was authenticated, send him to RemindersActivity
+        setContentView(R.layout.activity_authentication)
+        Log.d("ggg", "onCreate AuthenticationActivity")
 
 //          TODO: a bonus is to customize the sign in flow to look nice using :
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
 
-    /**
-     * Registers a launcher used to start the process of executing an ActivityResultContract
-     * so that we can listen to the result of the sign - in process
-     * This must be done in onCreate() or on Attach, ie before the fragment is created
-     */
-    private fun registerForSignInResult() {
-        registerForActivityResult = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()){ result: ActivityResult ->
-            val response = IdpResponse.fromResultIntent(result.data)
-            // Listen to the result of the sign - in process
-            if(result.resultCode == Activity.RESULT_OK){
-                Log.i(TAG, "User ${FirebaseAuth.getInstance().currentUser?.displayName} has signed in")
-            } else {
-                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
-            }
-        }
-    }
-
-    private fun launchSignInFlow() {
-        // Give users the option to either sign in / register with their email or Google account
-        val providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build())
-
-        registerForActivityResult.launch(AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build())
-
-    }
 }

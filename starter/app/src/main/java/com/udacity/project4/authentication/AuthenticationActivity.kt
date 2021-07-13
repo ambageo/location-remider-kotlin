790932f8c7ba5eee9b6056c140ce9950cadfb423
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-07-13 08:51:50 +0100

    Code cleanup

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,23 +1,9 @@
 package com.udacity.project4.authentication
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
-import com.udacity.project4.databinding.ActivityAuthenticationBinding
-
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the

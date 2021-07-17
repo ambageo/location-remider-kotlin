a90f61e2d8e19ea4f2eb85d8db3fdc5b713b4b17
George Ampartzidis <ambageo@gmail.com>
2021-07-17 13:09:47 +0100

    Test commit

--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,9 +1,11 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.udacity.project4.locationreminders.data.FakeDataSource
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -16,5 +18,8 @@ class RemindersListViewModelTest {
 
     private val reminder1 = ReminderDataItem("Reminder1", "Description1", "Location1", 10.0, 10.0)
     private val reminder2 = ReminderDataItem("Reminder2", "Description2", "Location2", 20.0, 20.0)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
 }

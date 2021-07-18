d7d2ed36e8c8da07a9737c62836e8483e59776bb
GeorgeAmpartzidis <george.ampartzidis@mpway.com>
2021-07-18 14:11:10 +0100

    Attemp to write test for ReminderListFragment

--- starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/reminderslist/ReminderListFragmentTest.kt
@@ -2,22 +2,42 @@ package com.udacity.project4.locationreminders.reminderslist
 
 import android.content.Context
 import android.os.Bundle
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.local.LocalDB
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.EspressoIdlingResource
+import com.udacity.project4.util.monitorFragment
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.GlobalContext
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.*
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
@@ -25,7 +45,88 @@ import org.mockito.Mockito.verify
 @MediumTest
 class ReminderListFragmentTest {
 
+    private lateinit var dataSource: ReminderDataSource
+
+    // An idling resource that waits for Data Binding to have no pending bindings.
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun initializeRepository(){
+        stopKoin()
+
+        /**
+        * Use Koin as a Service Locator
+        * */
+        val myModule = module {
+            viewModel {
+                RemindersListViewModel(getApplicationContext(), get() as ReminderDataSource)
+            }
+
+            single { RemindersLocalRepository(get()) }
+            single { LocalDB.createRemindersDao(getApplicationContext()) }
+        }
+
+        startKoin {
+            androidContext(getApplicationContext())
+            modules(listOf(myModule))
+        }
+
+        dataSource = GlobalContext.get().koin.get()
+        runBlocking {
+            dataSource.deleteAllReminders()
+        }
+        }
+
+    /**
+     * Idling resources tell Espresso that the app is idle or busy. This is needed when operations
+     * are not scheduled in the main Looper (for example when executed on a different thread).
+     * Whenever either of those Resources are busy, Espresso will wait until they are idle.
+     */
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    /**
+     * Unregister your Idling Resource so it can be garbage collected and does not leak any memory.
+     */
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    val reminder = ReminderDTO("title", "description", "location", 10.0, 10.0)
+
+    //    TODO: test the displayed data on the UI.
+    @Test
+    fun showRemindersInUI() = runBlockingTest {
+        // GIVEN a reminder
+        dataSource.saveReminder(reminder)
+
+        // WHEN Launching the fragment
+        val fragmentScenario = launchFragmentInContainer<ReminderListFragment>(Bundle.EMPTY, R.style.AppTheme)
+
+        // Monitor the change in the fragment
+        dataBindingIdlingResource.monitorFragment(fragmentScenario)
+
+        // THEN the reminder is displayed
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+        onView(withId(R.id.reminderTitle)).check(matches(withText("title")))
+        onView(withId(R.id.reminderDescription)).check(matches(withText("description")))
+        onView(withId(R.id.selectLocation)).check(matches(withText("location")))
+
+    }
+
+
+    }
+
+
+
 //    TODO: test the navigation of the fragments.
-//    TODO: test the displayed data on the UI.
+
 //    TODO: add testing for the error messages.
-}
